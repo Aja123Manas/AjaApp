a18c543468c0960c7c0f6dab9b64284d4bf073fc
Onsasepb <ajamanas436789@gmail.com>
2024-03-22 15:21:47 +0300

    initial commit

---
+++ app/src/main/java/com/example/ajaapp/LayoutActivity.kt
@@ -0,0 +1,170 @@
+package com.example.ajaapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.ajaapp.ui.theme.AjaAppTheme
+
+class LayoutActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Layout()
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Layout() {
+    Column(modifier = Modifier.fillMaxSize()) {
+
+        val mContext = LocalContext.current
+        //TopAppBar
+        TopAppBar(
+            title = {
+                Text(
+                    text = "Homescreen",
+                    color = Color.White
+                )
+            },
+            colors = TopAppBarDefaults.mediumTopAppBarColors(Color.Gray),
+            navigationIcon = {
+                IconButton(onClick = {
+                    mContext.startActivity(Intent(mContext,IntentActivity::class.java))
+                }
+                )
+                {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "Arrow Back",
+                        tint = Color.White
+                    )
+                }
+
+            },
+
+            actions = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Default.Menu,
+                        contentDescription = "share",
+                        tint = Color.White
+                    )
+                }
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Default.Menu,
+                        contentDescription = "settings",
+                        tint = Color.White
+                    )
+                }
+            }
+        )
+
+        //End of TopAppBar
+        Text(
+            text = "DESTINATIONS",
+            fontSize = 30.sp,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            fontFamily = FontFamily.Cursive
+        )
+
+        //First Row
+        Row {
+            Image(
+                painter = painterResource(id = R.drawable.ho2),
+                contentDescription = "Dubai",
+                modifier = Modifier.size(width = 200.dp, height = 200.dp)
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+
+            Column {
+
+                Text(text = "Dubai", fontWeight = FontWeight.Bold, fontSize = 20.sp)
+                Text(text = "Here are some best places to visit in Kenya: Read more. Image description. Bigger Group? Get special offers upto 50% off! We create unforgettable adventures ...")
+            }
+        }
+        Row {
+            Image(
+                painter = painterResource(id = R.drawable.mountain),
+                contentDescription = "Mt.Kenya",
+                modifier = Modifier.size(width = 200.dp, height = 200.dp)
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+
+            Column {
+                Text(text = "Kenya", fontWeight = FontWeight.Bold, fontSize = 20.sp)
+                Text(text = "Here are some best places to visit in Kenya: Read more. Image description. Bigger Group? Get special offers upto 50% off! We create unforgettable adventures ...")
+            }
+        }
+
+        Row {
+            Image(
+                painter = painterResource(id = R.drawable.colour),
+                contentDescription = "Newzealand",
+                modifier = Modifier.size(width = 200.dp, height = 200.dp)
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            Column {
+                Text(text = "Newzealand", fontWeight = FontWeight.Bold, fontSize = 20.sp)
+                Text(text = "Here are some best places to visit in Kenya: Read more. Image description. Bigger Group? Get special offers upto 50% off! We create unforgettable adventures ...")
+            }
+        }
+
+        //End of Row
+
+        Spacer(modifier = Modifier.height(50.dp))
+        Button(
+            onClick = { /*TODO*/ },
+            shape = RoundedCornerShape(10.dp),
+            colors = ButtonDefaults.buttonColors(Color.Cyan),
+            modifier = Modifier
+                .fillMaxWidth()
+
+        ) {
+            Text(text = "Next")
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+  fun LayoutPreview() {
+    Layout()
+}
--- app/src/main/java/com/example/ajaapp/MainActivity.kt
+++ app/src/main/java/com/example/ajaapp/MainActivity.kt
@@ -1,19 +1,36 @@
 package com.example.ajaapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -39,91 +56,106 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Demo(){
- Column(modifier = Modifier.fillMaxSize()) {
+ Column(
+
+
+         modifier = Modifier
+         .fillMaxSize()
+         .paint(painterResource(id = R.drawable.background),
+             contentScale = ContentScale.FillBounds))
+ {
+     val mContext = LocalContext.current
      Text(
          text = "Welcome to Android",
          fontSize = 30.sp,
-         color = Color.Red,
+         color = Color.Magenta,
          fontWeight = FontWeight.Bold,
          fontStyle = FontStyle.Italic,
-         textDecoration = TextDecoration.Underline)
-     Text(text = "Android software development is the process by which applications are created for devices running the Android operating system. Google states that \"Android ...\n" +
-             "\u200EOfficial development tools · \u200EAndroid Developer Challenge · \u200EJava standards\n" +
-             "\n")
-
-
-     Text(text = "Types of Cars",
+         textDecoration = TextDecoration.Underline
+     )
+     Text(text = "Android software development is the process by which applications are created for devices running the Android operating system. Google states that \"Android ...\n" + "\u200EOfficial development tools ·")
+     Text(
+         text = "Types of Cars",
          fontSize = 20.sp,
          modifier = Modifier
              .fillMaxWidth()
              .background(Color.Cyan)
-             .height(40.dp)
-         ,
+             .height(40.dp),
          textAlign = TextAlign.Center,
          color = Color.White,
-         fontWeight = FontWeight.Bold)
+         fontWeight = FontWeight.Bold
+     )
      Text(text = "1.Subaru")
      Text(text = "2.Mercedes Benz")
-     Text(text = "3.Audi")
-     Text(text = "4.Mazda")
 
 
-     Text(text = "Types of Androids",
-         fontSize = 20.sp,
-         modifier = Modifier
-             .fillMaxWidth()
-             .background(Color.Gray)
-             .height(40.dp)
-         ,
-       textAlign = TextAlign.Center,
-         color = Color.White,
-         fontWeight = FontWeight.Bold)
-     Text(text = "1.Smart Phones")
-     Text(text = "2.Smart Watches")
-     Text(text = "3.Tablets")
-     Text(text = "4.Laptops")
+             Text(
+                 text = "Types of Androids",
+                 fontSize = 20.sp,
+                 modifier = Modifier
+                     .fillMaxWidth()
+                     .background(Color.Cyan)
+                     .height(40.dp),
+                 textAlign = TextAlign.Center,
+                 color = Color.White,
+                 fontWeight = FontWeight.Bold
+             )
+             Text(text = "1.Smart Phones")
+             Text(text = "2.Smart Watches")
+
+             Button(
+                 onClick = { /*TODO*/ },
+                 shape = RoundedCornerShape(5.dp),
+                 colors = ButtonDefaults.buttonColors(Color.Cyan),
+                 modifier = Modifier
+                     .padding(start = 100.dp, end = 100.dp)
+
+             )
+
+             {
+                 Text(text = "More Devices")
+
+             }
+             Divider()
+             Text(
+                 text = "eMobolis Mobile Training Institute",
+                 fontSize = 20.sp,
+                 fontWeight = FontWeight.Bold
+             )
+
+             Box(
+                 modifier = Modifier.fillMaxWidth(),
+                 contentAlignment = Alignment.Center
+             ) {
+                 Image(
+                     painter = painterResource(id = R.drawable.ho2),
+                     contentDescription = "hotel",
+                     modifier = Modifier
+                         .size(200.dp)
+                         .clip(shape = CircleShape),
+                     contentScale = ContentScale.Crop
 
 
+                 )
 
 
-     Text(text = "Types of Programming Languages",
-         fontSize = 20.sp,
-         modifier = Modifier
-             .fillMaxWidth()
-             .background(Color.Magenta)
-             .height(40.dp)
-         ,
-         textAlign = TextAlign.Center,
-         color = Color.White,
-         fontWeight = FontWeight.Bold)
-     Text(text = "1.Python")
-     Text(text = "2.C++")
-     Text(text = "3.JavaScript")
-     Text(text = "4.Kotlin")
+             }
 
-
-
-     Text(text = "Types of Computer Packages",
-         fontSize = 20.sp,
-         modifier = Modifier
-             .fillMaxWidth()
-             .background(Color.Red)
-             .height(40.dp)
-         ,
-         textAlign = TextAlign.Center,
-         color = Color.White,
-         fontWeight = FontWeight.Bold)
-     Text(text = "1.DataBase")
-     Text(text = "2.Excel")
-     Text(text = "3.Word Processor")
-     Text(text = "4.Publisher")
-
-
-
- }
-}
-@Preview(showBackground = true)
-@Composable
-fun DemoPreview(){
-    Demo()
-}
+             Button(onClick = {
+                              mContext.startActivity(Intent(mContext,LayoutActivity::class.java))
+             },
+                 shape = RoundedCornerShape(5.dp),
+                 colors = ButtonDefaults.buttonColors(Color.Cyan),
+                 modifier = Modifier
+                     .fillMaxWidth()
+                     .padding(start = 30.dp, end = 30.dp)
+             ) {
+                 Text(text = "Continue")
+             }
+         }
+     }
+     @Preview(showBackground = true)
+     @Composable
+     fun DemoPreview() {
+         Demo()
+     }
